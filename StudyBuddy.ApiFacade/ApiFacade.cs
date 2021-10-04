79737a6a175cb23588190133cb289b01c97ff486
Andreas Caldewei <68024440+AndreasCaldewei@users.noreply.github.com>
2021-10-04 18:02:47 +0200

    Makes the dev url in the debug mode to localhost and in the run to production mode

--- StudyBuddy.ApiFacade/ApiFacade.cs
+++ StudyBuddy.ApiFacade/ApiFacade.cs
@@ -1,11 +1,18 @@
 ﻿using StudyBuddy.ApiFacade.Services;
+using Xamarin.Essentials;
 
 namespace StudyBuddy.ApiFacade
 {
     public class ApiFacade : IApi
     {
-        public static string base_url = "https://localhost:5001/";
-        //private readonly string base_url = "https://studybuddy.hshl.de/";
+
+        
+#if DEBUG
+        private readonly string base_url = DeviceInfo.Platform == DevicePlatform.Android ? "https://10.0.2.2:5001/" : "https://localhost:5001/";
+#else 
+        private readonly string base_url = "https://studybuddy.hshl.de/";
+#endif
+
 
         public ApiFacade()
         {
