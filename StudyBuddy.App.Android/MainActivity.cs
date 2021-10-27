9420b7c6bcbbf1e3403147608d70b70b59f4b00b
Sratschka <sascha.turner@stud.hshl.de>
2021-10-27 15:49:36 +0200

    App-Icons eingeführt

--- StudyBuddy.App.Android/MainActivity.cs
+++ StudyBuddy.App.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Platform = Xamarin.Essentials.Platform;
 
 namespace App.Droid
 {
-    [Activity(Label = "StudyBuddy", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+    [Activity(Label = "StudyBuddy", Theme = "@style/MainTheme", MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                                ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : FormsAppCompatActivity
