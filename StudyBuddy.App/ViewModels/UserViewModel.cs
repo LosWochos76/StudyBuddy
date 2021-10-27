c93625bbf748b54be9510a7e1366e5d36b798e0d
Alexander Stuckenholz <stuckenholz@iMac-von-Alexander.local>
2021-10-27 11:43:08 +0200

    Fixed little bug

--- StudyBuddy.App/ViewModels/UserViewModel.cs
+++ StudyBuddy.App/ViewModels/UserViewModel.cs
@@ -7,13 +7,14 @@ namespace StudyBuddy.App.ViewModels
 {
     public class UserViewModel : INotifyPropertyChanged
     {
+        private int count_of_common_friends = 0;
+
         public int ID { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Nickname { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
-        public int CountOfCommonFriends { get; set; }
 
         public bool IsAdmin => Role == Role.Admin;
         public bool IsStudent => Role == Role.Student;
@@ -31,6 +32,7 @@ namespace StudyBuddy.App.ViewModels
                 NotifyPropertyChanged("NotRequestedForFriendship");
             }
         }
+
         public bool RequestedForFriendship { get { return FriendshipRequest != null; } }
         public bool NotRequestedForFriendship { get { return FriendshipRequest == null; } }
 
@@ -69,6 +71,16 @@ namespace StudyBuddy.App.ViewModels
 
             var keywords = Helper.SplitIntoKeywords(search_text);
             return Helper.ContainsAny(Firstname + Lastname + Nickname + Email, keywords);
+        }
+
+        public int CountOfCommonFriends
+        {
+            get { return count_of_common_friends; }
+            set
+            {
+                count_of_common_friends = value;
+                NotifyPropertyChanged();
+            }
         }
     }
 }
