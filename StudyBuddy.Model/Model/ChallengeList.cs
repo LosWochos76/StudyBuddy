a9e4b3543f5b1d606848637123b76bfcc65a0b64
Alex <alexander.stuckenholz@gmail.com>
2022-01-25 11:57:27 +0100

    Replaced name StudyBuddy by Gameuction in the backend

---
+++ StudyBuddy.Model/Model/ChallengeList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StudyBuddy.Model.Model
+{
+    public class ChallengeList
+    {
+        public int Count { get; set; }
+        public IEnumerable<Challenge> Objects { get; set; }
+    }
+}
