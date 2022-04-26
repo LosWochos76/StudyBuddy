9593c036bd70454656531c911d8c726c769729cc
Paul H <paul.hartung@stud.hshl.de>
2022-04-26 18:16:41 +0200

    lots of design updates + first try for restart button

--- StudyBuddy.App/ViewModels/StatisticsViewModel.cs
+++ StudyBuddy.App/ViewModels/StatisticsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using StudyBuddy.App.Api;
+﻿using System.ComponentModel;
+using StudyBuddy.App.Api;
 using StudyBuddy.App.Misc;
 using StudyBuddy.App.Views;
 using StudyBuddy.Model;
@@ -11,37 +12,56 @@ using Xamarin.Forms;
 
 namespace StudyBuddy.App.ViewModels
 {
-    public class StatisticsViewModel : ViewModelBase
+    public class StatisticsViewModel : ViewModelBase, INotifyPropertyChanged
     {
-        private UserStatistics _userStatistic;
         public UserStatistics UserStatistics { get { return _userStatistic; } set { _userStatistic = value; NotifyPropertyChanged(); } }
-        private BarChart _totalPointsChart;
+        private UserStatistics _userStatistic;
         public BarChart TotalPointsChart { get { return _totalPointsChart; } set { _totalPointsChart = value; NotifyPropertyChanged(); } }
-        private DonutChart _totalChallengesChart;
+        private BarChart _totalPointsChart;
         public DonutChart TotalChallengesChart { get { return _totalChallengesChart; } set { _totalChallengesChart = value; NotifyPropertyChanged(); } }
-        public IAsyncCommand RefreshCommand { get; }
+        private DonutChart _totalChallengesChart;
         public bool IsRefreshing { get; set; }
-        public ICommand TotalChallengesCommand { get; set; }
+        public IAsyncCommand RefreshCommand { get; }
+        public IAsyncCommand TotalChallengesCommand { get; set; }
+        public IAsyncCommand TotalBadgeCommand { get; set; }
+        public Color ThemeColor 
+        { 
+            get 
+            { 
+                switch (Application.Current.UserAppTheme)
+                {
+                    case OSAppTheme.Light: return Color.FromHex("#F2F2F7");
+                    case OSAppTheme.Dark: return Color.Transparent;
+                    default: return Color.Transparent;
+                }
+            }
+        }
 
         public StatisticsViewModel(IApi api, IDialogService dialog, INavigationService navigation) : base(api, dialog, navigation)
         {
-            this.TotalChallengesCommand = new Command(ShowCompletedChallenges);
+            TotalChallengesCommand = new AsyncCommand(ShowCompletedChallenges);
+            TotalBadgeCommand = new AsyncCommand(ShowTotalBadge);
             RefreshCommand = new AsyncCommand(RefreshView);
         }
 
-        private void ShowCompletedChallenges()
+        private async Task ShowCompletedChallenges()
         {
-            Navigation.Push(new ChallengesCompletedPage());
+            await Navigation.Push(new ChallengesCompletedPage());
+        }
+        
+        private async Task ShowTotalBadge()
+        {
+            await Navigation.Push(new TotalBadgePage());
         }
 
         private async Task RefreshView()
         {
-            Refresh();
+            await Refresh();
             IsRefreshing = false;
             NotifyPropertyChanged(nameof(IsRefreshing));
         }
 
-        public async void Refresh()
+        public async Task Refresh()
         {
             try
             {
@@ -89,8 +109,8 @@ namespace StudyBuddy.App.ViewModels
                     Color = SKColor.Parse("#007AFF")
                 },
             };
-            TotalPointsChart = new BarChart {Entries = pointsEntries, LabelTextSize = 0, BackgroundColor = SKColor.Parse("#F2F2F7") ,ValueLabelOrientation = Orientation.Horizontal, ValueLabelTextSize = 40, BarAreaAlpha = 0};
-            TotalChallengesChart = new DonutChart {Entries = challengesEntries, BackgroundColor = SKColor.Parse("#F2F2F7")};
+            TotalPointsChart = new BarChart {Entries = pointsEntries, LabelTextSize = 0, BackgroundColor = new SKColor(0,0,0,0) ,ValueLabelOrientation = Orientation.Horizontal, ValueLabelTextSize = 40, BarAreaAlpha = 0};
+            TotalChallengesChart = new DonutChart {Entries = challengesEntries, BackgroundColor = new SKColor(0,0,0,0)};
         }
     }
 }
