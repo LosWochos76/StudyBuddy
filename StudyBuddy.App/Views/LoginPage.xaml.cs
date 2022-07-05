2ff4996d3979d320eb1acf9c0354e9b5517f3e17
Paul Hartung <paul.hartung@stud.hshl.de>
2022-07-05 20:25:36 +0200

    deutsch - englisch text fix

--- StudyBuddy.App/Views/LoginPage.xaml.cs
+++ StudyBuddy.App/Views/LoginPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using StudyBuddy.App.ViewModels;
 using TinyIoC;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace StudyBuddy.App.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
         private readonly LoginViewModel view_model;
