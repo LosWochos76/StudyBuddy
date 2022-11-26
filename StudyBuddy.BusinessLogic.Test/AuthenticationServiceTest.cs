7138098741b0ea189c459e5e083c41a7e0792bfa
Stefan Riedel <Stefan-Felix.Riedel@stud.hshl.de>
2022-11-26 15:10:46 +0100

    Login function in AuthenticationService tested

--- StudyBuddy.BusinessLogic.Test/AuthenticationServiceTest.cs
+++ StudyBuddy.BusinessLogic.Test/AuthenticationServiceTest.cs
@@ -71,6 +71,39 @@ namespace StudyBuddy.BusinessLogic.Test
             string token1 = backend.AuthenticationService.GenerateUserToken(user1.Email);
 
             Assert.True(backend.AuthenticationService.CheckPasswordResetToken(token1, user1.PasswordHash));
+            Assert.False(backend.AuthenticationService.CheckPasswordResetToken("randomString", user1.PasswordHash));
+        }
+
+        [Fact]
+        public void LoginTest()
+        {
+            var user1 = new User() { ID = 1, Email = "test@test.de", Password = "test", AccountActive = false};
+            user1 = backend.UserService.Insert(user1);
+
+            UserCredentials uc1 = new UserCredentials();
+            uc1.EMail = user1.Email;
+            uc1.Password = user1.Password;
+
+            var LR = backend.AuthenticationService.Login(uc1);
+            //7 = AccountDisabled
+            Assert.Equal(7, (int)LR.Status);
+
+            user1.AccountActive = true;
+            LR = backend.AuthenticationService.Login(uc1);
+            //1 = EmailNotVerified
+            Assert.Equal(1, (int)LR.Status);
+
+            user1.EmailConfirmed= true;
+            LR = backend.AuthenticationService.Login(uc1);
+            //0 = Success
+            Assert.Equal(0, (int)LR.Status);
+
+            UserCredentials uc2 = new UserCredentials();
+            uc2.EMail = "tada@bla.de";
+            uc2.Password= "Tee";
+            LR = backend.AuthenticationService.Login(uc2);
+            //3 = UserNotFound
+            Assert.Equal(3, (int)LR.Status);
         }
     }
 }
---
+++ StudyBuddy.BusinessLogic.Test/BusinessEventServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using StudyBuddy.BusinessLogic.Test.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StudyBuddy.BusinessLogic.Test
+{
+    public class BusinessEventServiceTest
+    {
+        private IBackend backend;
+
+        public BusinessEventServiceTest()
+        {
+            var repository = new RepositoryMock();
+            backend = new Backend(repository);
+        }
+
+        [Fact]
+        public void DeleteTest()
+        { 
+        
+        }
+    }
+}
